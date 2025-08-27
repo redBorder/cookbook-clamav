eebfe5887c83dc0efe3ec4370cfa0586b9e67f39
Daniel Castro <dcastro@redborder.com>
2025-08-27 12:08:52 +0100

    change execution order, lint fixes and add safe access to attributes in template

--- resources/providers/config.rb
+++ resources/providers/config.rb
@@ -15,38 +15,6 @@ action :add do
       end
     end
 
-    service 'clamav-freshclam' do
-      service_name 'clamav-freshclam'
-      ignore_failure true
-      supports status: true, reload: true, restart: true, enable: true
-      action :nothing
-    end
-
-    service 'clamd@scan' do
-      service_name 'clamd@scan'
-      ignore_failure true
-      supports status: true, reload: true, restart: true, enable: true
-      action :nothing
-    end
-
-    template "/etc/clamd.d/scan.conf" do
-      source "clamd.conf.erb"
-      owner user
-      group user
-      mode '0644'
-      retries 2
-      notifies :restart, 'service[clamd@scan]'
-    end
-
-    template "/etc/freshclam.conf" do
-      source "clamscan_freshclam.conf.erb"
-      owner user
-      group user
-      mode '0644'
-      retries 2
-      notifies :restart, 'service[clamav-freshclam]'
-    end
-
     directory '/var/lib/clamav' do
       owner 'clamupdate'
       group 'clamupdate'
@@ -63,20 +31,46 @@ action :add do
       action :create
     end
 
-    unless File.exist?("/var/lib/clamav/daily.cld")
-      execute "update_clamav_database" do
-          ignore_failure true
-          command "/usr/bin/freshclam --config-file /etc/freshclam.conf"
-          action :run
-      end
+    template "/etc/clamd.d/scan.conf" do
+      cookbook 'rb-clamav'
+      source "clamd.conf.erb"
+      owner user
+      group user
+      mode '0644'
+      retries 2
+      notifies :restart, 'service[clamd@scan]', :delayed
     end
-  end
+
+    template "/etc/freshclam.conf" do
+      cookbook 'rb-clamav'
+      source "clamscan_freshclam.conf.erb"
+      owner user
+      group user
+      mode '0644'
+      retries 2
+      notifies :restart, 'service[clamav-freshclam]', :delayed
+    end
+
+    execute 'initial_freshclam' do
+      command '/usr/bin/freshclam --config-file /etc/freshclam.conf --stdout'
+      only_if do
+        sigs = Dir['/var/lib/clamav/*.{cvd,cld}']
+        sigs.empty? || sigs.any? { |f| (Time.now - ::File.mtime(f)) > 24*3600 }
+      end
+      notifies :restart, 'service[clamd@scan]', :delayed
+    end
 
     service 'clamav-freshclam' do
+      service_name 'clamav-freshclam'
+      ignore_failure true
+      supports status: true, reload: true, restart: true, enable: true
       action [:enable, :start]
     end
 
     service 'clamd@scan' do
+      service_name 'clamd@scan'
+      ignore_failure true
+      supports status: true, reload: true, restart: true, enable: true
       action [:enable, :start]
     end
 
